c5674023d70916d6948a506e8c1f01de1dd6fc5c
Sambulo Senda <admin@users-MacBook-Air.local>
2020-08-27 15:53:50 +0100

    reactapp

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>Airbnb clone! No rights reserved</p>
+            <p>privacy . Terms . Sitemap . Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
